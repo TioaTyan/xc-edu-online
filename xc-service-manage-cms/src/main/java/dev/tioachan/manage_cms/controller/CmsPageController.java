26994ee57a803984fd600ff154943c71d70ec517
TioaChan <xiaozhidiguo@gmail.com>
2020-08-15 18:02:50 +0800

    Update CmsPageController.java

--- xc-service-manage-cms/src/main/java/dev/tioachan/manage_cms/controller/CmsPageController.java
+++ xc-service-manage-cms/src/main/java/dev/tioachan/manage_cms/controller/CmsPageController.java
@@ -8,10 +8,7 @@ import dev.tioachan.framework.model.response.QueryResponseResult;
 import dev.tioachan.framework.model.response.QueryResult;
 import dev.tioachan.manage_cms.service.PageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
